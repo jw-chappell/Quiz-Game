e347af24ea6b5a9ec677c1f466699224f25a83c8
jw-chappell <jwchappel98@gmail.com>
2021-10-19 02:32:41 -0400

    5/10 Finished DC Questions

--- data/comics.js
+++ data/comics.js
@@ -1,14 +1,44 @@
 const comicsQuestions = [
   {
     question : "Which Flash created the Speed Force",
-    correctAnswer : "Barry Allen",
+    correctAnswer : 1,
     choices : ["Wally West","Barry Allen","Jay Garrick","Bart Allen"]
   } ,
 
   {
     question : "",
-    correctAnswer : "",
+    correctAnswer : ,
     choices : [""]
+  } ,
+
+  {
+    question : "What Robin was killed by the Joker? ",
+    correctAnswer : 2,
+    choices : ["Tim Drake", "Dick Grayson", "Jason Todd","Damian Wayne"]
+  } ,
+
+  {
+    question : "",
+    correctAnswer : ,
+    choices : [""]
+  } ,
+
+  {
+    question : "What is Wonder Woman's 'earth name'?",
+    correctAnswer : 0,
+    choices : ["Diana Prince", "Jessica Prince", "Nicole Prince", "Dana Prince"]
+  } ,
+
+  {
+    question : "",
+    correctAnswer : ,
+    choices : [""]
+  } ,
+
+  {
+    question : "Which superhero gained the powers of ancient gods by a Wizard? ",
+    correctAnswer : 2,
+    choices : ["Superman", "Plastic Man", "Shazam", "Martian Manhunter" ]
   } ,
 
   {
@@ -18,39 +48,9 @@ const comicsQuestions = [
   } ,
 
   {
-    question : "",
-    correctAnswer : "",
-    choices : [""]
-  } ,
-
-  {
-    question : "",
-    correctAnswer : "",
-    choices : [""]
-  } ,
-
-  {
-    question : "",
-    correctAnswer : "",
-    choices : [""]
-  } ,
-
-  {
-    question : "",
-    correctAnswer : "",
-    choices : [""]
-  } ,
-
-  {
-    question : "",
-    correctAnswer : "",
-    choices : [""]
-  } ,
-
-  {
-    question : "",
-    correctAnswer : "",
-    choices : [""]
+    question : "What is the name of the terrifying version of the Joker and Batman in the dark multiverse of Earth-22? ",
+    correctAnswer : 1,
+    choices : ["The Killer Bat","The Batman Who Laughs","The Jokerman","Serious Clown"]
   } ,
 
   {
